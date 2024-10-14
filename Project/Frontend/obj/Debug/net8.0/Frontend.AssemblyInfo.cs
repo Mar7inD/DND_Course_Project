ea6b9b8b44c0dd08b47b679f5d98a9de3f448f13
Plo4i <midag@abv.bg>
2024-10-14 14:31:58 +0200

    Update gitignore

--- Project/Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
+++ Project/Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478250f5f56b460482f97de3d52dbfb865bfdf71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82410a350605819a5cde1fd83ea35d9733311fa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
